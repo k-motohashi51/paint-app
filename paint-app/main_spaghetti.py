5d2a35a2fa4649683d1f78768e5300b5a02c0794
koichi <k.motohashi51@gmail.com>
2022-12-09 23:17:09 +0900

    latest

--- paint-app/main_spaghetti.py
+++ paint-app/main_spaghetti.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 from enum import Enum
 from tkinter import filedialog
-from PIL import ImageTk, Image
+from PIL import ImageTk
 
 def main():
     app = Application()
@@ -34,47 +34,63 @@ class Application:
         self.create_widgits()
         self.root.mainloop()
 
+
 class Menu:
     def __init__(self):
         self.menu_bar = None
         self.file = None
 
+    # メニューウィジェットを作成する
     def create_menu(self, root, canvas_class):
         self.menu_bar = tk.Menu(root)
         root.config(menu = self.menu_bar)
-
         self.file = tk.Menu(self.menu_bar)
         self.menu_bar.add_cascade(label = "File", menu = self.file)
-        self.file.add_command(label = "New File", command = lambda: canvas_class.new_canvas())
-        self.file.add_command(label = "Open Image", command = lambda: self.open_image(canvas_class))
-        self.file.add_command(label = "Save as ...", command = lambda: self.file_save_as(canvas_class))
+        self.file.add_command(
+            label = "New File",
+            command = lambda: canvas_class.new_canvas()
+        )
+        self.file.add_command(
+            label = "Open Image",
+            command = lambda: self.open_image(canvas_class)
+        )
+        self.file.add_command(
+            label = "Save as ...",
+            command = lambda: self.file_save_as(canvas_class)
+        )
         self.file.add_separator()
-        self.file.add_command(label = "Exit")
+        self.file.add_command(
+            label = "Exit",
+            command = lambda: self.exit_app(root)
+        )
     
+    # 画像を読み込む
     def open_image(self, canvas_class, event = None):
-        file_name = filedialog.askopenfilename(title = "Open file", defaultextension = '.png')
-        print(file_name)
-        #read_image = Image.open(file_name)
-        #photo_image = tk.PhotoImage(image = read_image)
-        #canvas_class.open_image_on_canvas(photo_image)
+        file_name = filedialog.askopenfilename(
+            title = "Open file",
+            defaultextension = '.png'
+        )
         img = ImageTk.PhotoImage(file = file_name)
         canvas_class.open_image_on_canvas(img)
     
+    # 名前を付けて保存
     def file_save_as(self, canvas_class, event = None):
-        file_name = filedialog.asksaveasfilename(title = "Save with filename", defaultextension = '.ps')
+        file_name = filedialog.asksaveasfilename(
+            title = "Save with filename",
+            defaultextension = '.ps'
+        )
         canvas_class.save_file(file_name)
+    
+    # アプリケーションを終了する
+    def exit_app(self, root):
+        root.destroy()
     
 
 class Canvas:
     def __init__(self):
         self.f_canvas = None    # frame_canvas
         self.w_canvas = None    # widgit_canvas
-        # self.is_clicked = None
-        # self.is_being_dragged = None
         self.current_id = -1    # 操作中の図形のID
-        self.color = "black"
-        self.thickness = None
-        self.mode = Mode.PEN
         self.image = None
     
     def create_canvas(self, root):
@@ -82,13 +98,22 @@ class Canvas:
         self.create_widgits()
     
     def create_frame(self, root):
-        self.f_canvas = tk.Frame(root, bg = "#CCFF99", pady = 10, padx = 10)
-        self.f_canvas.place(relx = 0.0, rely = 0.0, relwidth = 0.7, relheight = 1.0)
+        self.f_canvas = tk.Frame(
+            root, bg = "#CCFF99", 
+            padx = 10, pady = 10
+        )
+        self.f_canvas.place(
+            relx = 0.0, rely = 0.0,
+            relwidth = 0.7, relheight = 1.0
+        )
     
     def create_widgits(self):
         print("created")
         self.w_canvas = tk.Canvas(self.f_canvas, bg = "white")
-        self.w_canvas.place(relx = 0.0, rely = 0.1, relwidth = 1.0, relheight = 0.8)
+        self.w_canvas.place(
+            relx = 0.0, rely = 0.1,
+            relwidth = 1.0, relheight = 0.8
+        )
         self.w_canvas.bind("<Button-1>", self.on_key_left)
         self.w_canvas.bind("<B1-Motion>", self.on_key_left_dragging)
     
@@ -97,9 +122,13 @@ class Canvas:
 
     def on_key_left(self, event):
         if Tool.mode == Mode.PEN or Tool.mode == Mode.ERASER:
-            self.current_id = self.w_canvas.create_line(event.x, event.y, event.x, event.y, fill = Tool.color, width = Tool.thickness)
+            self.current_id = self.w_canvas.create_line(
+                event.x, event.y,
+                event.x, event.y,
+                fill = Tool.color,
+                width = Tool.thickness.get()
+            )
         
-    
     def on_key_left_dragging(self, event):
         points = self.w_canvas.coords(self.current_id)
         points.extend([event.x, event.y])
@@ -107,16 +136,17 @@ class Canvas:
 
     # Canvasの内容をファイルとして保存
     def save_file(self, file_name):
-        print("file_name = " + file_name)
         self.w_canvas.postscript(file = file_name, colormode = "color")
     
     def open_image_on_canvas(self, img):
-        print("display Img:" , self.w_canvas.winfo_width()/2 , " : ", self.w_canvas.winfo_height()/2)
-        #self.w_canvas.create_image(50, 50, image = img)
-        self.image = self.image = img
-        self.w_canvas.create_image(0,0, image = self.image, anchor = tk.NW)
+        self.image = img
+        self.w_canvas.create_image(
+            0, 0, 
+            image = self.image,
+            anchor = tk.NW
+        )
 
-        # 無地の新しいキャンバスに遷移
+    # 無地の新しいキャンバスに遷移
     def new_canvas(self):
         self.w_canvas.destroy()
         self.create_widgits()
@@ -128,18 +158,19 @@ class Mode(Enum):
     ERASER = 2
 
 class Tool:
-    # マングリング？
     mode = None
     color = "black"
-    thickness = 10
+    thickness = 5
 
     def __init__(self):
         # ウィジェット
         self.f_tool = None
         self.w_b_pen = None
-
-        # プロパティ
-        #self.thickness = None
+        self.w_thickbar = None
+        
+        self.img_pen = tk.PhotoImage(file = "pen_icon.png")
+        self.img_eraser = tk.PhotoImage(file = "eraser_icon.png")
+        Tool.thickness = tk.IntVar()
     
     def create_tool(self, root):
         self.create_frame(root)
@@ -147,25 +178,36 @@ class Tool:
 
     def create_frame(self, root):
         self.f_tool = tk.Frame(root, bg = "#FFFF99", pady = 10, padx = 10)
-        self.f_tool.place(relx = 0.7, rely = 0.0, relwidth = 0.3, relheight = 0.6)
+        self.f_tool.place(
+            relx = 0.7, rely = 0.0,
+            relwidth = 0.3, relheight = 0.6
+        )
     
     def create_widgits(self):
-        self.w_b_pen = tk.Button(self.f_tool, text = "Pen", command = lambda: self.on_button_click(Mode.PEN))
-        self.w_b_eraser = tk.Button(self.f_tool, text = "Eraser", command = lambda: self.on_button_click(Mode.ERASER))
+        self.w_b_pen = tk.Button(
+            self.f_tool,
+            text = "Pen",
+            image = self.img_pen,
+            command = lambda: self.on_button_click(Mode.PEN)
+        )
+        self.w_b_eraser = tk.Button(
+            self.f_tool,
+            text = "Eraser",
+            image = self.img_eraser,
+            command = lambda: self.on_button_click(Mode.ERASER)
+        )
+        self.w_thickbar = tk.Scale(
+            self.f_tool,
+            orient = tk.HORIZONTAL,
+            from_ = 1,
+            to = 30,
+            variable = Tool.thickness,
+            label = "Thickness"
+        )
+
         self.w_b_pen.pack(side = tk.TOP)
         self.w_b_eraser.pack(side = tk.TOP)
-    
-    #@staticmethod
-    #def get_mode(self):
-    #    return Tool.mode
-    
-    # ツールの選択状態を設定
-    #@staticmethod
-    #def get_property(self):
-    #    if self.mode == Mode.PEN:
-    #        return (color, self.thickness)
-    #    if self.mode == Mode.ERASER:
-    #        return (self.color, self.thickness)
+        self.w_thickbar.pack(side = tk.TOP)
     
     def on_button_click(self, mode):
         self.mode = mode
@@ -180,8 +222,6 @@ class Tool:
             Tool.color = "white"
             self.w_b_eraser["state"] = tk.DISABLED
             self.w_b_pen["state"] = tk.NORMAL
-            # 色ボタンの無効化
-
         else:
             print("Argue Error")
 
